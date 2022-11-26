2df819294c64d3175a39a3c41022437a8da7fddb
Akeem Lawrence <akeemwlawrence@gmail.com>
2022-11-26 17:45:15 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = function () {
+
+}
+
+mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = function() {
+
+}
+
+companyCeo = 'Susan Smith';
